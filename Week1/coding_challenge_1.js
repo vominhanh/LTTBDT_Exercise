ec069fa826181618b30b1a0bb807752e4a1d9123
Minh Anh <avminh824@gmail.com>
2024-08-22 10:11:25 +0700

    first commit

---
+++ Week1/coding_challenge_1.js
@@ -0,0 +1,3 @@
+//1. Store Mark's and John's mass and height in variables
+//2. Calculate both their BMIs using the formula (you can even implement both versions)
+//3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
